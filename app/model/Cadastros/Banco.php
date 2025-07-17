1b6d99c7f05f94d5c6f41981b6d3cefb2b97b4d2
Guilherme <guilhermealvesmuller2000@gmail.com>
2025-07-17 16:02:53 -0300

    Adiciona constante CREATEDBY para rastrear autor de criação em Banco

--- app/model/Cadastros/Banco.php
+++ app/model/Cadastros/Banco.php
@@ -6,12 +6,15 @@ class Banco extends TRecord
     const PRIMARYKEY = 'id';
     const IDPOLICY = 'max'; // {max, serial}
 
+    const CREATEDBY  = 'usuario_id';
+
     public function __construct($id = NULL, $callObjectLoad = TRUE)
     {
         parent::__construct($id, $callObjectLoad);
         parent::addAttribute('agencia');
         parent::addAttribute('nome');
         parent::addAttribute('conta');
+        parent::addAttribute('usuario_id');
         parent::addAttribute('created_at');
         parent::addAttribute('updated_at');
     }
